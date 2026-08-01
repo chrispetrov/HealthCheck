dcf2b2f45374bf13babe09fae641e877450a968f
chrispetrov904 <chrispetrov904@gmail.com>
2026-08-01 13:35:55 +0200

    Added Home, FetchData, and NavMenu components

--- healthcheck.client/src/app/app-module.ts
+++ healthcheck.client/src/app/app-module.ts
@@ -4,18 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
+import { Home } from './home/home';
+import { FetchData } from './fetch-data/fetch-data';
 
 @NgModule({
-  declarations: [
-    App
-  ],
-  imports: [
-    BrowserModule, HttpClientModule,
-    AppRoutingModule
-  ],
-  providers: [
-    provideBrowserGlobalErrorListeners(),
-  ],
-  bootstrap: [App]
+  declarations: [App, Home, FetchData],
+  imports: [BrowserModule, HttpClientModule, AppRoutingModule],
+  providers: [provideBrowserGlobalErrorListeners()],
+  bootstrap: [App],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ healthcheck.client/src/app/fetch-data/fetch-data.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-fetch-data',
+  standalone: false,
+  templateUrl: './fetch-data.html',
+  styleUrl: './fetch-data.css',
+})
+export class FetchData {}
