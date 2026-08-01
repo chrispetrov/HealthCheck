87c2831e6ab026acf889897f93ca54ed9d4069b8
chrispetrov904 <chrispetrov904@gmail.com>
2026-08-01 13:38:22 +0200

    Actually added the nav-menu

--- healthcheck.client/src/app/app-module.ts
+++ healthcheck.client/src/app/app-module.ts
@@ -6,9 +6,10 @@ import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
 import { Home } from './home/home';
 import { FetchData } from './fetch-data/fetch-data';
+import { NavMenu } from './nav-menu/nav-menu';
 
 @NgModule({
-  declarations: [App, Home, FetchData],
+  declarations: [App, Home, FetchData, NavMenu],
   imports: [BrowserModule, HttpClientModule, AppRoutingModule],
   providers: [provideBrowserGlobalErrorListeners()],
   bootstrap: [App],
